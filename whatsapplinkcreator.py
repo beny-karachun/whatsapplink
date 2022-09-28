11bd3af4027fd5a6bcfccea174bbc0aa2d3a4428
beny-karachun <93853386+beny-karachun@users.noreply.github.com>
2022-09-28 19:01:17 +0300

    Add files via upload

---
+++ whatsapplinkcreator.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import countryinfo
+
+
+def whatsappAPImessage(message):
+   return message.replace(" ","%20")
+def whatsappAPInumber(number):
+    return str("972" + str(number[1:].replace(r"-",r"")))
+def whatsappAPIlink(number,message):
+    
+    return "https://wa.me/" + (whatsappAPInumber(number)) +  "?text=" + (whatsappAPImessage(message))
+
+
+number = st.text_input("Enter your phone number")
+message = st.text_input("Enter your custom message")
+submit = st.button("Generate")
+if submit:
+ st.success("Your custom link is: " + whatsappAPIlink(number,message))
+
