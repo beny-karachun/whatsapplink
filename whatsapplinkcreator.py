1790632dbfa6a1ef5be51e0248f9f0485c9c6b67
Beny Karachun <beny1703@gmail.com>
2022-09-28 20:25:00 +0300

    Fixed container

--- whatsapplinkcreator.py
+++ whatsapplinkcreator.py
@@ -11,14 +11,14 @@ def whatsappAPIlink(number,message):
     return "https://wa.me/" + (whatsappAPInumber(number)) +  "?text=" + (whatsappAPImessage(message))
 
 with st.container():
-    st.title("Custom WhatsApp message URL")
-    number = st.text_input("Enter your phone number")
-    message = st.text_input("Enter your custom message")
-    submit = st.button("Generate")
-    if submit and message and number:
-    st.success("Your custom link is: " + whatsappAPIlink(number,message))
-    if submit and message and not(number):
-        st.error("Fill out the number field!")
-    elif submit and number and not(message):
-        st.error("Fill out the message field!")
+ st.title("Custom WhatsApp message URL")
+ number = st.text_input("Enter your phone number")
+ message = st.text_input("Enter your custom message")
+ submit = st.button("Generate")
+ if submit and message and number:
+  st.success("Your custom link is: " + whatsappAPIlink(number,message))
+ if submit and message and not(number):
+    st.error("Fill out the number field!")
+ elif submit and number and not(message):
+    st.error("Fill out the message field!")
 
